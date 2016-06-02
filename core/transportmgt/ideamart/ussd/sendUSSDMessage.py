4aafdd6044fcd931fb336a2eb926c6e1bfaf44c1
tharinda <tharinda221@gmail.com>
2016-06-02 15:46:52 +0530

    USSD operations finished

--- core/transportmgt/ideamart/ussd/sendUSSDMessage.py
+++ core/transportmgt/ideamart/ussd/sendUSSDMessage.py
@@ -1,3 +1,6 @@
+import urllib
+import urllib2
+
 import requests
 from flask import json
 import logging
@@ -14,15 +17,19 @@ def sendUSSDMessage(messageBody):
            "encoding": messageBody.encording,
            "version": messageBody.version
            }
-    form_data = json.dumps(res)
+    # form_data = json.dumps(res)
     logging.error("Print payload")
-    logging.error(form_data)
+    # logging.error(form_data)
     logging.error("url: " + messageBody.url)
-    result = requests.post(url=messageBody.url, data=form_data)
+    #data = urllib.urlencode(res)
+    req = urllib2.Request(messageBody.url, data=json.dumps(res), headers={"Content-Type": "application/json", "Accept": "application/json"})
+    response = urllib2.urlopen(req)
+    result = response.read()
+    # result = requests.post(url=messageBody.url, data=form_data)
     logging.error("Result content")
-    logging.error(result.content)
+    logging.error(result)
 
-    if result.status_code == 200:
+    if response.getcode() == 200:
         logging.error('*** Message delivered Successfully! ****')
     else:
-        logging.error('*** Message was not delivered Successfully!! ERROR-CODE: ' + str(result.status_code) + ' ****')
+        logging.error('*** Message was not delivered Successfully!! ERROR-CODE: ' + str(response.getcode()) + ' ****')
